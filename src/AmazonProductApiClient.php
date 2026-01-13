4a363ae334537bd86e98bf1076381f326ae447fc
Alessio Carrafa <alessio.carrafa@hotmail.it>
2026-01-13 11:46:49 +0100

    feat: enhance AmazonProductApiClient with language support and new API methods

--- src/AmazonProductApiClient.php
+++ src/AmazonProductApiClient.php
@@ -22,7 +22,12 @@ class AmazonProductApiClient
     private const API_VERSION = '5.0';
     private const CONTENT_TYPE = 'application/json; charset=UTF-8';
     private const CONTENT_ENCODING = 'amz-1.0';
-    private const AMZ_TARGET = 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetItems';
+
+    // Operation targets
+    private const TARGET_GET_ITEMS = 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetItems';
+    private const TARGET_SEARCH_ITEMS = 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.SearchItems';
+    private const TARGET_GET_VARIATIONS = 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetVariations';
+    private const TARGET_GET_BROWSE_NODES = 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetBrowseNodes';
 
     private string $accessKey;
     private string $secretKey;
@@ -32,25 +37,26 @@ class AmazonProductApiClient
     private string $endpoint;
     private string $trackingPlaceholder;
     private int $timeout;
+    private string $language;
     private AwsSignature $awsSignature;
 
     /**
      * Marketplace endpoints configuration
      */
     private const MARKETPLACE_ENDPOINTS = [
-        'www.amazon.com' => ['endpoint' => 'webservices.amazon.com', 'region' => 'us-east-1'],
-        'www.amazon.ca' => ['endpoint' => 'webservices.amazon.ca', 'region' => 'us-east-1'],
-        'www.amazon.com.mx' => ['endpoint' => 'webservices.amazon.com.mx', 'region' => 'us-east-1'],
-        'www.amazon.co.uk' => ['endpoint' => 'webservices.amazon.co.uk', 'region' => 'eu-west-1'],
-        'www.amazon.de' => ['endpoint' => 'webservices.amazon.de', 'region' => 'eu-west-1'],
-        'www.amazon.fr' => ['endpoint' => 'webservices.amazon.fr', 'region' => 'eu-west-1'],
-        'www.amazon.it' => ['endpoint' => 'webservices.amazon.it', 'region' => 'eu-west-1'],
-        'www.amazon.es' => ['endpoint' => 'webservices.amazon.es', 'region' => 'eu-west-1'],
-        'www.amazon.co.jp' => ['endpoint' => 'webservices.amazon.co.jp', 'region' => 'us-west-2'],
-        'www.amazon.in' => ['endpoint' => 'webservices.amazon.in', 'region' => 'eu-west-1'],
-        'www.amazon.com.br' => ['endpoint' => 'webservices.amazon.com.br', 'region' => 'us-east-1'],
-        'www.amazon.com.au' => ['endpoint' => 'webservices.amazon.com.au', 'region' => 'us-west-2'],
-        'www.amazon.sg' => ['endpoint' => 'webservices.amazon.sg', 'region' => 'us-west-2'],
+        'www.amazon.com' => ['endpoint' => 'webservices.amazon.com', 'region' => 'us-east-1', 'language' => 'en_US'],
+        'www.amazon.ca' => ['endpoint' => 'webservices.amazon.ca', 'region' => 'us-east-1', 'language' => 'en_CA'],
+        'www.amazon.com.mx' => ['endpoint' => 'webservices.amazon.com.mx', 'region' => 'us-east-1', 'language' => 'es_MX'],
+        'www.amazon.co.uk' => ['endpoint' => 'webservices.amazon.co.uk', 'region' => 'eu-west-1', 'language' => 'en_GB'],
+        'www.amazon.de' => ['endpoint' => 'webservices.amazon.de', 'region' => 'eu-west-1', 'language' => 'de_DE'],
+        'www.amazon.fr' => ['endpoint' => 'webservices.amazon.fr', 'region' => 'eu-west-1', 'language' => 'fr_FR'],
+        'www.amazon.it' => ['endpoint' => 'webservices.amazon.it', 'region' => 'eu-west-1', 'language' => 'it_IT'],
+        'www.amazon.es' => ['endpoint' => 'webservices.amazon.es', 'region' => 'eu-west-1', 'language' => 'es_ES'],
+        'www.amazon.co.jp' => ['endpoint' => 'webservices.amazon.co.jp', 'region' => 'us-west-2', 'language' => 'ja_JP'],
+        'www.amazon.in' => ['endpoint' => 'webservices.amazon.in', 'region' => 'eu-west-1', 'language' => 'en_IN'],
+        'www.amazon.com.br' => ['endpoint' => 'webservices.amazon.com.br', 'region' => 'us-east-1', 'language' => 'pt_BR'],
+        'www.amazon.com.au' => ['endpoint' => 'webservices.amazon.com.au', 'region' => 'us-west-2', 'language' => 'en_AU'],
+        'www.amazon.sg' => ['endpoint' => 'webservices.amazon.sg', 'region' => 'us-west-2', 'language' => 'en_SG'],
     ];
 
     /**
@@ -63,6 +69,8 @@ class AmazonProductApiClient
         "ItemInfo.ProductInfo",
         "ItemInfo.TechnicalInfo",
         "ItemInfo.Features",
+        "ItemInfo.ContentRating",
+        "ItemInfo.ExternalIds",
         "Images.Primary.Small",
         "Images.Primary.Medium",
         "Images.Primary.Large",
@@ -83,7 +91,8 @@ class AmazonProductApiClient
         "OffersV2.Listings.MerchantInfo",
         "OffersV2.Listings.Availability",
         "OffersV2.Listings.Condition",
-        "OffersV2.Listings.LoyaltyPoints"
+        "OffersV2.Listings.LoyaltyPoints",
+        "OffersV2.Listings.ProgramEligibility.IsPrimeEligible"
     ];
 
 
@@ -108,16 +117,17 @@ class AmazonProductApiClient
         if (is_string($config)) {
             $config = $this->loadConfigFromFile($config);
         }
-        
+
         $this->validateConfig($config);
-        
+
         // Support both old format (access_key) and new format (accessKey)
         $this->accessKey = $config['accessKey'] ?? $config['access_key'];
         $this->secretKey = $config['secretKey'] ?? $config['secret_key'];
         $this->partnerTag = $config['partnerTag'] ?? $config['partner_tag'];
         $this->trackingPlaceholder = $config['trackingPlaceholder'] ?? $config['tracking_placeholder'] ?? 'booBLZTRKood';
         $this->timeout = $config['timeout'] ?? 30; // Default 30 seconds timeout
-        
+        $this->language = $config['language'] ?? null;
+
         // Handle marketplace/host configuration
         if (isset($config['host'])) {
             // New JSON format uses 'host' directly
@@ -129,7 +139,7 @@ class AmazonProductApiClient
             $marketplaceConfig = self::MARKETPLACE_ENDPOINTS[$this->marketplace];
             $this->endpoint = $marketplaceConfig['endpoint'];
         }
-        
+
         // Set region (use provided or default from marketplace)
         if (isset($config['region'])) {
             $this->region = $config['region'];
@@ -137,7 +147,13 @@ class AmazonProductApiClient
             $marketplaceConfig = self::MARKETPLACE_ENDPOINTS[$this->marketplace] ?? null;
             $this->region = $marketplaceConfig['region'] ?? 'us-east-1';
         }
-        
+
+        // Set default language if not provided
+        if (!$this->language) {
+            $marketplaceConfig = self::MARKETPLACE_ENDPOINTS[$this->marketplace] ?? null;
+            $this->language = $marketplaceConfig['language'] ?? 'en_US';
+        }
+
         $this->awsSignature = new AwsSignature($this->accessKey, $this->secretKey, $this->region);
     }
 
@@ -180,34 +196,125 @@ class AmazonProductApiClient
             'ItemIds' => $asins,
             'ItemIdType' => 'ASIN',
             'Marketplace' => $this->marketplace,
-            'LanguagesOfPreference'=> ["it_IT"],
+            'LanguagesOfPreference' => [$this->language],
             'PartnerTag' => $this->partnerTag,
             'PartnerType' => 'Associates',
             'OfferCount' => $offerCount,
             'Resources' => $resources
         ];
 
-        $response = $this->makeRequest($payload);
-        
+        $response = $this->makeRequest($payload, self::TARGET_GET_ITEMS, '/paapi5/getitems');
+
         return $this->parseItemsResponse($response);
+    }
+
+    /**
+     * Search products by keywords or categories
+     * 
+     * @param string $keywords Search keywords
+     * @param array $params Additional parameters (SearchIndex, BrowseNodeId, etc.)
+     * @param array $resources Optional resources to retrieve
+     * @return ProductItem[]
+     * @throws AmazonApiException
+     */
+    public function searchItems(string $keywords, array $params = [], array $resources = []): array
+    {
+        $resources = empty($resources) ? self::DEFAULT_RESOURCES : $resources;
+
+        $payload = array_merge([
+            'Keywords' => $keywords,
+            'Marketplace' => $this->marketplace,
+            'LanguagesOfPreference' => [$this->language],
+            'PartnerTag' => $this->partnerTag,
+            'PartnerType' => 'Associates',
+            'Resources' => $resources
+        ], $params);
+
+        $response = $this->makeRequest($payload, self::TARGET_SEARCH_ITEMS, '/paapi5/searchitems');
+
+        $items = $response['SearchResult']['Items'] ?? [];
+
+        $result = [];
+        foreach ($items as $itemData) {
+            $result[] = new ProductItem($itemData);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get variations of a product
+     * 
+     * @param string $asin Parent or Child ASIN
+     * @param array $resources Optional resources to retrieve
+     * @return ProductItem[]
+     * @throws AmazonApiException
+     */
+    public function getVariations(string $asin, array $resources = []): array
+    {
+        $resources = empty($resources) ? self::DEFAULT_RESOURCES : $resources;
+
+        $payload = [
+            'ASIN' => $asin,
+            'Marketplace' => $this->marketplace,
+            'LanguagesOfPreference' => [$this->language],
+            'PartnerTag' => $this->partnerTag,
+            'PartnerType' => 'Associates',
+            'Resources' => $resources
+        ];
+
+        $response = $this->makeRequest($payload, self::TARGET_GET_VARIATIONS, '/paapi5/getvariations');
+
+        $items = $response['VariationsResult']['Items'] ?? [];
+
+        $result = [];
+        foreach ($items as $itemData) {
+            $result[] = new ProductItem($itemData);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get Browse Nodes information
+     * 
+     * @param array $browseNodeIds Array of Browse Node IDs
+     * @param array $resources Optional resources to retrieve
+     * @return array Raw API response
+     * @throws AmazonApiException
+     */
+    public function getBrowseNodes(array $browseNodeIds, array $resources = []): array
+    {
+        $payload = [
+            'BrowseNodeIds' => $browseNodeIds,
+            'Marketplace' => $this->marketplace,
+            'LanguagesOfPreference' => [$this->language],
+            'PartnerTag' => $this->partnerTag,
+            'PartnerType' => 'Associates',
+            'Resources' => empty($resources) ? ['BrowseNodes.Ancestor', 'BrowseNodes.Children'] : $resources
+        ];
+
+        return $this->makeRequest($payload, self::TARGET_GET_BROWSE_NODES, '/paapi5/getbrowsenodes');
     }
 
     /**
      * Make HTTP request to Amazon API
      * 
      * @param array $payload Request payload
+     * @param string $target API Operation target
+     * @param string $uri API URI path
      * @return array Response data
      * @throws AmazonApiException
      */
-    private function makeRequest(array $payload): array
+    private function makeRequest(array $payload, string $target = self::TARGET_GET_ITEMS, string $uri = '/paapi5/getitems'): array
     {
-        $url = "https://{$this->endpoint}/paapi5/getitems";
+        $url = "https://{$this->endpoint}{$uri}";
         $jsonPayload = json_encode($payload, JSON_THROW_ON_ERROR);
-        
-        $headers = $this->buildHeaders($jsonPayload);
-        
+
+        $headers = $this->buildHeaders($jsonPayload, $target, $uri);
+
         $curl = curl_init();
-        
+
         curl_setopt_array($curl, [
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
@@ -228,7 +335,7 @@ class AmazonProductApiClient
         $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         $error = curl_error($curl);
         $errorCode = curl_errno($curl);
-        
+
         curl_close($curl);
 
         if ($response === false) {
@@ -248,13 +355,13 @@ class AmazonProductApiClient
         if ($httpCode !== 200) {
             $this->handleApiError($httpCode, $decodedResponse);
         }
-        
+
         // Handle API errors that come with HTTP 200 status but contain error information
         if (isset($decodedResponse['Errors']) && !empty($decodedResponse['Errors'])) {
             $error = $decodedResponse['Errors'][0];
             $errorMessage = $error['Message'] ?? 'Unknown API error';
             $errorCode = $error['Code'] ?? 'UNKNOWN_ERROR';
-            
+
             // Map common error codes to appropriate exception types
             switch ($errorCode) {
                 case 'InvalidParameterValue':
@@ -281,25 +388,27 @@ class AmazonProductApiClient
      * Build HTTP headers for the request
      * 
      * @param string $payload JSON payload
+     * @param string $target API Operation target
+     * @param string $uri API URI path
      * @return array Headers array
      */
-    private function buildHeaders(string $payload): array
+    private function buildHeaders(string $payload, string $target = self::TARGET_GET_ITEMS, string $uri = '/paapi5/getitems'): array
     {
         $timestamp = gmdate('Ymd\THis\Z');
         $contentSha256 = hash('sha256', $payload);
-        
+
         $headersForSigning = [
             'Host' => $this->endpoint,
             'Content-Type' => self::CONTENT_TYPE,
             'Content-Encoding' => self::CONTENT_ENCODING,
-            'X-Amz-Target' => self::AMZ_TARGET,
+            'X-Amz-Target' => $target,
             'X-Amz-Content-Sha256' => $contentSha256,
             'X-Amz-Date' => $timestamp,
         ];
-        
+
         $authHeader = $this->awsSignature->generateAuthorizationHeader(
             'POST',
-            '/paapi5/getitems',
+            $uri,
             $payload,
             $timestamp,
             $headersForSigning
@@ -309,7 +418,7 @@ class AmazonProductApiClient
             'Host: ' . $this->endpoint,
             'Content-Type: ' . self::CONTENT_TYPE,
             'Content-Encoding: ' . self::CONTENT_ENCODING,
-            'X-Amz-Target: ' . self::AMZ_TARGET,
+            'X-Amz-Target: ' . $target,
             'X-Amz-Content-Sha256: ' . $contentSha256,
             'X-Amz-Date: ' . $timestamp,
             'Authorization: ' . $authHeader,
@@ -327,7 +436,7 @@ class AmazonProductApiClient
     {
         // Handle different possible response structures
         $items = null;
-        
+
         if (isset($response['ItemsResult']['Items'])) {
             $items = $response['ItemsResult']['Items'];
         } elseif (isset($response['GetItemsResponse']['Items'])) {
@@ -335,7 +444,7 @@ class AmazonProductApiClient
         } elseif (isset($response['Items'])) {
             $items = $response['Items'];
         }
-        
+
         if ($items === null) {
             // If we got here but have errors in the response, it means there was an API error
             // that should have been caught earlier but wasn't properly handled
@@ -345,7 +454,7 @@ class AmazonProductApiClient
                 $errorCode = $error['Code'] ?? 'UNKNOWN_ERROR';
                 throw new AmazonApiException("API Error ({$errorCode}): {$errorMessage}");
             }
-            
+
             // Debug the actual response structure
             throw new AmazonApiException('Invalid response format. Response structure: ' . json_encode(array_keys($response)));
         }
@@ -397,16 +506,16 @@ class AmazonProductApiClient
         $accessKey = $config['accessKey'] ?? $config['access_key'] ?? null;
         $secretKey = $config['secretKey'] ?? $config['secret_key'] ?? null;
         $partnerTag = $config['partnerTag'] ?? $config['partner_tag'] ?? null;
-        
+
         // Check for required fields
         if (empty($accessKey)) {
             throw new InvalidParameterException("Missing required configuration: accessKey (or access_key)");
         }
-        
+
         if (empty($secretKey)) {
             throw new InvalidParameterException("Missing required configuration: secretKey (or secret_key)");
         }
-        
+
         if (empty($partnerTag)) {
             throw new InvalidParameterException("Missing required configuration: partnerTag (or partner_tag)");
         }
@@ -414,7 +523,7 @@ class AmazonProductApiClient
         // Check for marketplace or host
         $marketplace = $config['marketplace'] ?? null;
         $host = $config['host'] ?? null;
-        
+
         if (empty($marketplace) && empty($host)) {
             throw new InvalidParameterException("Missing required configuration: marketplace or host");
         }
@@ -440,8 +549,8 @@ class AmazonProductApiClient
     {
         $productItem = $this->getItem($asin, $resources, $offerCount);
         return new AmazonItem(
-            $productItem, 
-            $partnerTag ?? $this->partnerTag, 
+            $productItem,
+            $partnerTag ?? $this->partnerTag,
             $trackingPlaceholder ?? $this->trackingPlaceholder
         );
     }
@@ -460,16 +569,16 @@ class AmazonProductApiClient
     public function getAmazonItems(array $asins, array $resources = [], int $offerCount = 1, string $partnerTag = null, string $trackingPlaceholder = null): array
     {
         $productItems = $this->getItems($asins, $resources, $offerCount);
-        
+
         $amazonItems = [];
         foreach ($productItems as $productItem) {
             $amazonItems[] = new AmazonItem(
-                $productItem, 
-                $partnerTag ?? $this->partnerTag, 
+                $productItem,
+                $partnerTag ?? $this->partnerTag,
                 $trackingPlaceholder ?? $this->trackingPlaceholder
             );
         }
-        
+
         return $amazonItems;
     }
 
@@ -537,6 +646,23 @@ class AmazonProductApiClient
     }
 
     /**
+     * Get instance of Creators API (from creatorsapi-php-sdk)
+     * 
+     * @param array $config Creators API configuration (credentialId, credentialSecret, version)
+     * @return \Amazon\CreatorsAPI\v1\com\amazon\creators\api\DefaultApi
+     */
+    public function getCreatorsApi(array $config = []): \Amazon\CreatorsAPI\v1\com\amazon\creators\api\DefaultApi
+    {
+        $creatorsConfig = new \Amazon\CreatorsAPI\v1\Configuration();
+
+        if (isset($config['credentialId'])) $creatorsConfig->setCredentialId($config['credentialId']);
+        if (isset($config['credentialSecret'])) $creatorsConfig->setCredentialSecret($config['credentialSecret']);
+        if (isset($config['version'])) $creatorsConfig->setVersion($config['version']);
+
+        return new \Amazon\CreatorsAPI\v1\com\amazon\creators\api\DefaultApi(null, $creatorsConfig);
+    }
+
+    /**
      * Set request timeout in seconds
      * 
      * @param int $timeout Timeout in seconds (minimum 1, maximum 300)
@@ -547,7 +673,7 @@ class AmazonProductApiClient
         if ($timeout < 1 || $timeout > 300) {
             throw new InvalidParameterException('Timeout must be between 1 and 300 seconds');
         }
-        
+
         $this->timeout = $timeout;
     }
 
@@ -559,5 +685,15 @@ class AmazonProductApiClient
     public function getTimeout(): int
     {
         return $this->timeout;
+    }
+
+    /**
+     * Get current language
+     * 
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->language;
     }
 }
